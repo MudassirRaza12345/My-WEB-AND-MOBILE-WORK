fe336929a005e4443b68c86a096863d986f357e1
MudassirRaza12345 <mudassirraza12345@gmail.com>
2020-08-18 14:46:35 -0700

    first commit

---
+++ filing task/Data Gridview/Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Data_Gridview
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            DriveInfo[] list = DriveInfo.GetDrives();
+            foreach (DriveInfo c in list)
+                comboBox1.Items.Add(c.Name);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 b = new Form1();
+            b.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = comboBox1.Text + "\\" + textBox1.Text;
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                MessageBox.Show("Folder has been  Created");
+            }
+            else
+            {
+                MessageBox.Show("Folder has not been  Created ");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ filing task/Data Gridview/Form8.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Data_Gridview
+{
+    public partial class Form8 : Form
+    {
+        public Form8()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 i = new Form1();
+            i.Show();
+            this.Hide();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sur = textBox1.Text;
+            string distination = textBox2.Text;
+            bool exists = System.IO.Directory.Exists(sur);
+            if(exists)
+            {
+                System.IO.Directory.CreateDirectory(distination);
+              
+            }
+            MessageBox.Show(" File has been copy ");
+        }
+
+        private void Form8_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog f = new FolderBrowserDialog();
+            f.ShowDialog();
+            textBox1.Text = f.SelectedPath;
+        }
+    }
+}
